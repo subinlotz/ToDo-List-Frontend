55cca933d34d1697d189bc6d36860b0f4930ab4e
subinlotz <lotzsubin@gmail.com>
2025-02-03 21:16:34 +0530

    Update HandleApi.js

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://todo-list-backend-nwr9.onrender.com"
+const API_BASE_URL = "https://todo-list-backend-fjya.onrender.com"
 
 const getTodo = (setTodo) => {
     axios
